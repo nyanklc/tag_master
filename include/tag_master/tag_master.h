aa3fae1e283b167ca03551206a2baaa1eba9c2a0
nyanklc <noyanerdinkilic@gmail.com>
2023-03-02 13:14:14 +0300

    base-derived

--- include/tag_master/tag_master.h
+++ include/tag_master/tag_master.h
@@ -22,24 +22,14 @@ namespace tag_master
   public:
     TagMaster();
 
-    void addDetector(std::shared_ptr<tag_detection::DetectorBase> det)
+    template <class T>
+    void addDetector(std::shared_ptr<T> det)
     {
       detectors_.push_back(det);
     }
 
-    template <class T>
-    void enableDetector(std::string name)
-    {
-      std::shared_ptr<T> det = std::dynamic_pointer_cast<T>(findDetector(name));
-      det->enable();
-    }
-
-    template <class T>
-    void disableDetector(std::string name)
-    {
-      std::shared_ptr<T> det = std::dynamic_pointer_cast<T>(findDetector(name));
-      det->disable();
-    }
+    void enableDetector(std::string name);
+    void disableDetector(std::string name);
 
     /* TODO: Implement */
     // template <class T>
@@ -49,34 +39,9 @@ namespace tag_master
     // void stopContinuous(std::string name);
     // void stopContinuousAll();
 
-    template <class T>
-    void runSingle(std::string name, cv::Mat &frame)
-    {
-      // TODO: maybe try without dynamic casting?
-      std::shared_ptr<T> det = std::dynamic_pointer_cast<T>(findDetector(name));
-      // ROS_INFO("found detector: %s, its type: %s", det->getName().c_str(), typeid(det).name());
-      if (det == nullptr)
-      {
-        ROS_WARN("Can't run a detector that doesn't exist.");
-        return;
-      }
-      // ROS_INFO("calling process");
-      std::cout << "#######################################\n";
-      std::cout << "process output: " << det->process(frame) << std::endl;
-      return;
-    }
-
-    template <class T>
-    tag_detection::DetectionOutput getOutput(std::string name)
-    {
-      std::shared_ptr<T> det = std::dynamic_pointer_cast<T>(findDetector(name));
-      return det->output();
-    }
-
-    std::shared_ptr<tag_detection::DetectorBase> getDetector(std::string name)
-    {
-      return findDetector(name);
-    }
+    void runSingle(std::string name, cv::Mat &frame);
+    tag_detection::DetectionOutput getOutput(std::string name);
+    std::shared_ptr<tag_detection::DetectorBase> getDetector(std::string name);
 
   private:
     std::shared_ptr<tag_detection::DetectorBase> findDetector(std::string name);
